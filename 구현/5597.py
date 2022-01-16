9722b2cefd38a82f04e15a7c9b975aa8f926401c
Jaewoo Gwak <jaewoo010207@gmail.com>
2022-01-16 20:19:12 +0900

    Create 5597.py

---
+++ 구현/5597.py
@@ -0,0 +1,13 @@
+# 5597 과제 안 내신 분..?
+# https://www.acmicpc.net/problem/5597
+
+arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+
+for i in range(28):
+    N = int(input());
+    arr.remove(N);
+    
+print(arr[0]);
+print(arr[1]);
+    
+    
