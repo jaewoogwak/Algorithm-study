da8677cd93ca3b735c53e9a6f005b161c4afd3b5
jaewoogwak <jaewoo010207@gmail.com>
2023-02-13 02:57:08 +0900

    9095 1,2,3 더하기

---
+++ 백준/DP/9095.py
@@ -0,0 +1,23 @@
+# 9095 1,2,3 더하기
+# https://www.acmicpc.net/problem/9095
+
+import sys
+
+sys.stdin = open("test.txt","rt")
+
+T = int(input())
+
+dp = [0 for _ in range(12)]
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 12):
+  dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for _ in range(T):
+  n = int(input())
+  print(dp[n])
+
+  
